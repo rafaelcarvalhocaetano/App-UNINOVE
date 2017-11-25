a0d9d9d6d609bfbaae3588cc27cfff70e599647f
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2017-11-25 14:49:39 -0200

    app help uninove

---
+++ CalculadoraUNINOVE/app/src/main/java/correcao/br/estudo/calculadorauninove/CalculadoraSimples.java
@@ -0,0 +1,20 @@
+package correcao.br.estudo.calculadorauninove;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by fernanda on 25/11/2017.
+ */
+
+public class CalculadoraSimples extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.calculadora_simples);
+    }
+}
---
+++ CalculadoraUNINOVE/app/src/main/java/correcao/br/estudo/calculadorauninove/SegundoGrau.java
@@ -0,0 +1,8 @@
+package correcao.br.estudo.calculadorauninove;
+
+/**
+ * Created by rafaelcarvalho on 25/11/2017.
+ */
+
+public class SegundoGrau {
+}
