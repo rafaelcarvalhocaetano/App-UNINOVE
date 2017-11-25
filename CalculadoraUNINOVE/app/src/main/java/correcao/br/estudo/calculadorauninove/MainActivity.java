c694b733bb86af993d3fad0f4c16273bb144ab8a
Rafael Carvalho Caetano <rapha.pse@outlook.com>
2017-11-25 15:21:37 -0200

    app help uninove

--- CalculadoraUNINOVE/app/src/main/java/correcao/br/estudo/calculadorauninove/MainActivity.java
+++ CalculadoraUNINOVE/app/src/main/java/correcao/br/estudo/calculadorauninove/MainActivity.java
@@ -1,13 +1,33 @@
 package correcao.br.estudo.calculadorauninove;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSimples, btn2grau;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        btnSimples = (Button) findViewById(R.id.btn1);
+        btn2grau = (Button) findViewById(R.id.btn2);
+
+
+        btnSimples.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivities(new Intent(MainActivity.this, CalculadoraSimples.class));
+            }
+        });
     }
+
+
+
 }
--- CalculadoraUNINOVE/app/src/main/java/correcao/br/estudo/calculadorauninove/SegundoGrau.java
+++ CalculadoraUNINOVE/app/src/main/java/correcao/br/estudo/calculadorauninove/SegundoGrau.java
@@ -1,8 +1,19 @@
 package correcao.br.estudo.calculadorauninove;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
 /**
- * Created by rafaelcarvalho on 25/11/2017.
+ * Created by fernanda on 25/11/2017.
  */
 
-public class SegundoGrau {
+public class SegundoGrau extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.segundo_grau);
+    }
 }
